4479384c6cf6f0fc9950574322be955fe292b876
boris <borisllonalonso@gmail.com>
2020-03-04 18:22:08 +0100

    login and register

--- WebProjectMovie/apps/main/urls.py
+++ WebProjectMovie/apps/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from apps.main import views
 
 urlpatterns = [
-	path('main', views.main, name='main'),
-    path('<int:pk>', views.main, name='main')
+	path('', views.main, name='main'),
+    path('register', views.register, name='register'),
+    path('login', views.loginn, name='login'),
+    #path('<int:pk>', views.main, name='main')
 ]
--- WebProjectMovie/apps/main/views.py
+++ WebProjectMovie/apps/main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Movies
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 def main(request):
-	return render(request, 'main.html', {})
+	return render(request, 'main.html')
 
 def movie_list(request):
 	movies = Movies.objects.all()
@@ -12,3 +14,25 @@ def movie_list(request):
 	}
 
 	return render(request, 'main.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main')
+    else:
+        form = UserCreationForm()
+
+    context = {'form' : form}
+    return render(request, 'register.html', context)
+
+def loginn(request):
+    #context = {'form' : form}
+    context = {}
+    return render(request, 'login.html', context)
