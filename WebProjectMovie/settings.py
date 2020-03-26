706005944bf473b1f9f5c0626afbf5978af92135
Mateus Daemon <mateusjdm@gmail.com>
2020-03-26 17:19:23 +0100

    Changing static root again for static files

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'url_login'
 
