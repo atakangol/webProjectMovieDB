404c61a679473bca5ae8473ab19c99d90a5b86df
Mateus Daemon <mateusjdm@gmail.com>
2020-03-26 17:01:26 +0100

    Testing heroku

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'url_login'
 
