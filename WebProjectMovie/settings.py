1c9ead7d825fc3d0b85cace610e09e7f024f0306
Mateus Daemon <mateusjdm@gmail.com>
2020-03-26 17:39:33 +0100

    Whitenoise integrate for static files

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'WebProjectMovie.urls'
@@ -122,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'url_login'
 
