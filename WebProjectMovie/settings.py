433e53e8add244de746307044acfa327423f9a1a
Mateus Daemon <mateusjdm@gmail.com>
2020-03-26 17:11:53 +0100

    import mime types for heroku tests

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, mimetypes
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +126,4 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'url_login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+mimetypes.add_type("text/css", ".css", True)
