ef61dda8e2dfdcd61a8020b3c7e4beb92ba075ef
mjunyent <marcjunyent1999@gmail.com>
2020-05-01 17:32:00 +0200

    behave ok - apps.main fails

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -32,11 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.main',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
