c807613b529b8a994290c18d991d03599de76ab8
mjunyent <marcjunyent1999@gmail.com>
2020-05-04 15:01:59 +0200

    userName deleted from models Favorite_movie, not working with admin

--- apps/main/models.py
+++ apps/main/models.py
@@ -49,11 +49,11 @@ class Casting(models.Model):
 
 class Favorite_Movie(models.Model):
     movieID = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    UserName = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    #movieName = models.CharField(max_length=30)
+    #UserName = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movieName = models.CharField(max_length=30, null=True)
 
     def __str__(self):
-        return self.movieID
+        return str(self.movieID)
 
     def get_absolute_url(self):
         return reverse('movies:movie_detail', kwargs={'pk': self.pk})
