bdbf127d5b45948839d0350267324c30c2044313
Mateus Daemon <mateusjdm@gmail.com>
2020-05-07 14:56:44 +0200

    testing heroku

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -128,7 +128,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'apps/main/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
