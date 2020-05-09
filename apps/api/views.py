c2f8274c7549351246c123e1f715d913f1e8f6e4
boris <borisllonalonso@gmail.com>
2020-05-09 12:52:16 +0200

    button request v2

--- apps/api/views.py
+++ apps/api/views.py
@@ -8,3 +8,14 @@ def check_admin(user):
 @user_passes_test(check_admin)
 def ReadApiView(request):    
     return render(request, 'readapi.html')
+
+def run_script(request):
+   if request.method == 'POST' and 'run_script' in request.POST:
+
+    # import function to run
+    from api_trials.api2db.py import #METHODS
+
+    #We can call the functions
+
+    # That is for if we want to redirect a user to a another page. 
+    # return HttpResponseRedirect(reverse(app_name:view_name)
