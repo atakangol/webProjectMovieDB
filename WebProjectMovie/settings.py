a83acb8d122618e1eb10bd08e65585eeea099b98
Mateus Daemon <mateusjdm@gmail.com>
2020-05-10 21:46:41 +0200

    heroku

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'url_login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
