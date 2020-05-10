d99bdb789047e11e537e141c1f388b1958e64f0c
mjunyent <marcjunyent1999@gmail.com>
2020-05-10 12:56:20 +0200

    heroku test

--- WebProjectMovie/settings.py
+++ WebProjectMovie/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'apps/main/static/main'),
